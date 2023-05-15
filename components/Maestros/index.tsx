2819b3891a3acb6686b100129408819a4e67f2f1
karen-Desktop <karen5808266@gmail.com>
2023-05-15 11:54:01 -0500

    responsive

--- components/Maestros/index.tsx
+++ components/Maestros/index.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import bolso from '../../public/img/bolso.png';
-import artesano9 from '../../public/img/artesano9.png'
-import artesano15 from '../../public/img/artesano15.png'
-import enmascarar from '../../public/img/Enmascarar grupo 18.png'
-import enmascarar1 from '../../public/img/Enmascarar grupo 19.png'
+import artesano9 from '../../public/img/artesano9.png';
+import artesano15 from '../../public/img/artesano15.png';
+import enmascarar from '../../public/img/Enmascarar grupo 18.png';
+import enmascarar1 from '../../public/img/Enmascarar grupo 19.png';
 
 import style from './style.module.css';
 const Maestro = () => {
@@ -15,10 +15,21 @@ const Maestro = () => {
 				</div>
 				<div className={style.contentSection}>
 					<div className={style.divImages}>
-						<Image src={enmascarar1} alt="bolso" />
-						<Image src={enmascarar} alt="bolso" />
-						<Image src={artesano9} alt="bolso" />
-						<Image src={artesano15} alt="bolso" />
+						<div className={style.img1}>
+							<Image src={enmascarar1} alt="bolso" />
+						</div>
+						<div className={style.img2}>
+							<Image src={enmascarar} alt="bolso" />
+						</div>
+						<div className={style.img3}>
+							<Image src={bolso} alt="bolso" />
+						</div>
+						<div className={style.img4}>
+							<Image src={artesano9} alt="bolso" />
+						</div>
+						<div className={style.img5}>
+							<Image src={artesano15} alt="bolso" />
+						</div>
 					</div>
 					<div className={style.divTexts}>
 						<h5>Maestros Artesanos</h5>
@@ -26,7 +37,6 @@ const Maestro = () => {
 							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam quibusdam quisquam
 							obcaecati facere fugit ipsa optio. Lorem ipsum dolor sit, amet consectetur adipisicing
 							elit. Eum deserunt dolores necessitatibus doloremque laudantium reprehenderit, optio
-							
 						</p>
 					</div>
 				</div>
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -13,14 +13,19 @@ const Navbar = () => {
 					<li>CRÉDITOS</li>
 					<li>FLY UP</li>
 				</div>
-					<div className={style.menuMovil}>
-						-
-					</div>
+				<div className={style.menuMovil}>
+					<Image src={folder + 'menu.svg'} alt="menu" width={50} height={50} />
+				</div>
 				<div className={style.titlelogo}>
 					<h3>VÉLEZ</h3>
 				</div>
 				<div className={style.contentInt}>
-					<input type="search" placeholder="¿QUÉ BUSCAS?" className={style.input} />
+					<input type="search" placeholder="¿QUÉ BUSCAS?" />
+					<Image src={folder + 'lupa.svg'} alt="lupa" width={30} height={30} />
+					<Image src={folder + 'user.svg'} alt="user" width={25} height={25} />
+					<Image src={folder + 'bolsa.svg'} alt="bolsa" width={25} height={25} />
+				</div>
+				<div className={style.contentMovil}>
 					<Image src={folder + 'lupa.svg'} alt="lupa" width={30} height={30} />
 					<Image src={folder + 'user.svg'} alt="user" width={25} height={25} />
 					<Image src={folder + 'bolsa.svg'} alt="bolsa" width={25} height={25} />
